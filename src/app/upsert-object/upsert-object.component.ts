10c22e3c42172a1c46d3eaf4a56d8bcdd70b416e
Yves Staudenmaier <yves.staudenmaier@gmail.com>
2019-10-09 14:29:38 +0200

    upsert/put componente finished

--- src/app/upsert-object/upsert-object.component.ts
+++ src/app/upsert-object/upsert-object.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {HttpRestService, Order, POSITIONS} from '../../services/http-rest.service';
+import {FormBuilder, FormGroup} from '@angular/forms';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-upsert-object',
@@ -7,9 +10,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpsertObjectComponent implements OnInit {
 
-  constructor() { }
+  orderForm: FormGroup;
+  positions = POSITIONS; // const from data service -- not good but works!
+
+  constructor(private http: HttpRestService, private formBuilder: FormBuilder, private router: Router) {
+    this.createForm();
+  }
 
   ngOnInit() {
+  }
+
+  createForm() {
+    this.orderForm = this.formBuilder.group({
+      id: ['-1'],
+      name: [''],
+      forename: [''],
+      positions: [''],
+      street: [''],
+      zip: [''],
+      town: [''],
+    });
+  }
+
+  // handle submit
+  onSubmit() {
+    if (this.orderForm.status === 'VALID') {
+      console.log(this.orderForm.value);
+      // no error. form data is valid. now create recipe and add it.
+      const order: Order = {
+        id: this.orderForm.value.id,
+        name: this.orderForm.value.name,
+        forename: this.orderForm.value.forename,
+        position: this.orderForm.value.positions,
+        street: this.orderForm.value.street,
+        zip: this.orderForm.value.zip,
+        town: this.orderForm.value.town
+      };
+      // PUT
+      this.http.putOrder(order).subscribe(data => console.log(data));
+    } else {
+      // reset all input fields due to a invalid recipe form status
+      this.orderForm.reset();
+    }
+  }
+
+  onCancel(): void {
+    this.orderForm.reset();
+    this.router.navigate(['/']);
   }
 
 }
